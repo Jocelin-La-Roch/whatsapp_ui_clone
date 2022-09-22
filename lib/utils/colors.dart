33c6adc5c59b928a2b0b8d9833a5ce7e61569e22
Jocelin-La-Roch <jocelinlaroch08@gmail.com>
2022-09-22 01:41:12 +0100

    set colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/rendering.dart';
+
+class UtilsColors {
+  static Color tealGreen = const Color.fromRGBO(18, 140, 126, 1);
+  static Color tealGreenDark = const Color.fromRGBO(7, 94, 84, 1);
+  static Color lightGreen = const Color.fromRGBO(37, 211, 102, 1);
+  static Color blue = const Color.fromRGBO(52, 183, 241, 1);
+}
